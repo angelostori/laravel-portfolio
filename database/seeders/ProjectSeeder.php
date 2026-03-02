d4fcdd44402cfbbabbf1b15188f0bef62810f506
angelostori <angelostori13@gmail.com>
2026-03-02 19:00:53 +0100

    Add ProjectSeeder to seed project data in the database

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+
+        for ($i = 0; $i < 10; $i++) {
+            $newProject = new Project();
+
+            $newProject->name = $faker->sentence(3);
+            $newProject->client = $faker->company();
+            $newProject->period = $faker->date();
+            $newProject->description = $faker->paragraph();
+
+            $newProject->save();
+        }
+    }
+}
