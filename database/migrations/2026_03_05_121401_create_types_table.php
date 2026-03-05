4e13c0432e75ca98c9e87d6b5fb7bcdd7ae4001b
angelostori <angelostori13@gmail.com>
2026-03-05 14:54:52 +0100

    make a migration to create a new table types

--- database/migrations/2026_03_05_121401_create_types_table.php
+++ database/migrations/2026_03_05_121401_create_types_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('projects', function (Blueprint $table) {
-            $table->string('type')->after('description');
+        Schema::create('types', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+
+            $table->timestamps();
         });
     }
 
@@ -21,8 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('projects', function (Blueprint $table) {
-            $table->dropColumn('type');
-        });
+        Schema::dropIfExists('types');
     }
 };
